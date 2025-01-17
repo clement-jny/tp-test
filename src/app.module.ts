b352b39350445db142eee84cdc4d35484673b5f8
clement-jny <clementjaunay@free.fr>
2025-01-17 13:47:24 +0100

    git commit -m "feat: generate users | install typeorm for sqlite"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+
+const orm = TypeOrmModule.forRoot({
+  type: 'sqlite',
+  database: 'db.sqlite',
+  entities: [__dirname + '/**/entities/*.entity{.ts,.js}'],
+  synchronize: true,
+});
 
 @Module({
-  imports: [],
+  imports: [UsersModule, orm],
   controllers: [AppController],
   providers: [AppService],
 })
