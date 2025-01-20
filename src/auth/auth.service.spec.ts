5e495cc6cf421cd8acb76c553ec6d532e7400750
clement-jny <clementjaunay@free.fr>
2025-01-20 22:56:14 +0100

    feat: create auth service + test

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { User } from '../users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let usersService: UsersService;
+
+  const usersRepository = {
+    create: jest.fn(),
+    save: jest.fn(),
+    // findOne: jest.fn(),
+    // find: jest.fn(),
+  } as unknown as Repository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: usersRepository,
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    usersService = module.get<UsersService>(UsersService);
   });
 
+  afterEach(() => jest.clearAllMocks());
+
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  describe('HashPassword', () => {
+    it('should not return empty', () => {
+      expect(authService.hashPassword('abc')).not.toBeFalsy();
+    });
+
+    it('should hash correctly', () => {
+      const password: string = 'abc';
+
+      const newPassword = authService.hashPassword(password);
+
+      expect(password).not.toEqual(newPassword);
+    });
   });
 });
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+// import { JwtService } from '@nestjs/jwt'; // install
+// import * as bcrypt from 'bcrypt'; // install
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  saltOrRounds: number = 10;
+
+  constructor(
+    private usersService: UsersService,
+    // private jwtService: JwtService,
+  ) {}
+
+  async signIn(createUserDto: CreateUserDto) {
+    // payload: CreateUserDto
+    //     const user = await this.usersService.findOneBy(email);
+    //     if (user?.password !== pass) {
+    //     const isMatch = await bcrypt.compare(password, user?.password);
+    //     if (!isMatch) {
+    //       throw new UnauthorizedException();
+    //     }
+    //     const payload = { sub: user.id, email: user.email };
+    //     return {
+    //       access_token: await this.jwtService.signAsync(payload),
+    //     };
+  }
+
+  async signUp(createUserDto: CreateUserDto) {
+    // const user = await this.usersService.create(payload);
+    //     const hashPass = await bcrypt.hash(payload.password, this.saltOrRounds)
+    //     let data = {
+    //       ...payload,
+    //       password: hashPass
+    //     }
+    //     const user = await this.usersService.create(data);
+    //     return user;
+  }
+
+  hashPassword(password: string) {
+    return password;
+  }
+}
